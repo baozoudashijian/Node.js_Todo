adfba011060dabcd8a8b3c90741f9b97f3c68223
Roger Zhang <1787009521@qq.com>
2022-04-23 22:30:58 +0800

    feat: 编辑功能

--- index.js
+++ index.js
@@ -96,28 +96,68 @@ program.command('mark')
                 selectOptions = JSON.parse(fileData).map((task, index) => {
                     return { name: task.content, value: index, checked: task.done ? true : false }
                 })
-            
-            inquirer
-                .prompt([
-                    {
-                        type: 'checkbox',
-                        message: 'Select tasks',
-                        name: 'tasks',
-                        choices: [
-                            new inquirer.Separator(' ==== The Tasks ==== '),
-                            ...selectOptions
-                        ]
-                    }])
-                .then(({tasks}) => {
-                    let res = JSON.parse(fileData)
-                    tasks.map((taskIndex) => {
-                        res[taskIndex].done = true
-                    }) 
-                    write(JSON.stringify(res))
-                })
+
+                inquirer
+                    .prompt([
+                        {
+                            type: 'checkbox',
+                            message: 'Select tasks',
+                            name: 'tasks',
+                            choices: [
+                                new inquirer.Separator(' ==== The Tasks ==== '),
+                                ...selectOptions
+                            ]
+                        }])
+                    .then(({ tasks }) => {
+                        let res = JSON.parse(fileData)
+                        tasks.map((taskIndex) => {
+                            res[taskIndex].done = true
+                        })
+                        write(JSON.stringify(res))
+                    })
             }
         })
 
+    });
+
+program.command('edit')
+    .description('edit a task')
+    .action((index) => {
+        let selectOptions = []
+        read().then((data) => {
+            let fileData = data.toString()
+            if (fileData) {
+                selectOptions = JSON.parse(fileData).map((task, index) => {
+                    return { name: task.content, value: index }
+                })
+                inquirer
+                    .prompt([
+                        {
+                            type: 'list',
+                            name: 'taskIndex',
+                            message: 'Whick task you want to change?',
+                            choices: [
+                                ...selectOptions
+                            ],
+                        },
+                    ])
+                    .then(({ taskIndex }) => {
+                        console.log(taskIndex)
+                        inquirer.
+                            prompt([
+                                {
+                                    type: 'input',
+                                    name: 'text',
+                                    message: "Enter the name you want to change",
+                                }
+                            ]).then(({ text }) => {
+                                let res = JSON.parse(fileData)
+                                res[taskIndex].content = text
+                                write(JSON.stringify(res))
+                            });
+                    });
+            }
+        })
     });
 
 program.parse();
